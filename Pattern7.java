6f1194ef96b07783d1f8d446698f9c2b2fdcf0d3
Srikanth_Attacker <fireboy024@gmail.com>
2023-01-30 09:07:19 +0530

    Pattern

---
+++ Pattern7.java
@@ -0,0 +1,24 @@
+package basics;
+
+public class Pattern7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1;i<=6;i++) 
+		{
+			for(int col=1;col<=i;col++)
+			{
+				System.out.print("*");
+			}
+			for(int colnum=1;colnum<=5;colnum++)
+			{
+				System.out.print(colnum);
+			}
+			System.out.println();
+		
+			  
+		}
+		
+	}
+}
